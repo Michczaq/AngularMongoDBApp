826691a50aabd27b3e7bc36c423dce2a7d500c29
Michal Gwizdala <gwizdalamichal@wp.pl>
2019-02-28 15:24:25 +0100

    initial commit

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const User = require('./model/user');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const url = 'mongodb://localhost/angularDB'; 
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended : false}))
+
+app.get('/api/user/login', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/api/user/login', (req, res) => {
+    mongoose.connect(url , { useNewUrlParser: true }, function(err){
+        if(err) throw err;
+        User.find({
+            username : req.body.username, password : req.body.password
+        }, function(err, user){
+            if(err) throw err;
+            if(user.length === 1){  
+                return res.status(200).json({
+                    status: 'success',
+                    data: user
+                })
+            } else {
+                return res.status(200).json({
+                    status: 'fail',
+                    message: 'Login Failed'
+                })
+            }
+             
+        })
+    });
+})
+ 
+
+app.listen(3000, () => console.log('blog server running on port 3000!'))
